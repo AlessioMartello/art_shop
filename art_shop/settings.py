97eeff2460a00ed67e6ccd0ea4e4308452146a6f
Alessio Martello <alessio.martello@hotmail.co.uk>
2021-08-27 11:20:26 +0100

    Ready for deployment to Heroku

--- art_shop/settings.py
+++ art_shop/settings.py
@@ -130,3 +130,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Heroku settings
+import django-heroku
+django-heroku.settings(locals())
