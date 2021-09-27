88845f3e384bb9f0be66bc1db68a955f5bdeb124
Alessio Martello <alessio.martello@hotmail.co.uk>
2021-09-27 19:52:12 +0100

    Styling and html changes

--- aless_art_shop/urls.py
+++ aless_art_shop/urls.py
@@ -1,7 +1,8 @@
 """Defines URL patterns for aless_art_shop app"""
 
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 app_name = "aless_art_shop"
@@ -10,4 +11,4 @@ urlpatterns = [
     path('product/<int:id>/', views.product, name='product'),
     path('about/', views.about, name="about"),
     path('faqs/', views.faqs, name="faqs")
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
